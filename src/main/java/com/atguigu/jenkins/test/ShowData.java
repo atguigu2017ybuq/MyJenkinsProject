85d735804aa4910bf9fa9383892ae4ff2acdf1d5
fengjie_global <fengjie_global@atguigu.com>
2017-08-04 14:33:57 +0800

    test scheduler

--- src/main/java/com/atguigu/jenkins/test/ShowData.java
+++ src/main/java/com/atguigu/jenkins/test/ShowData.java
@@ -2,7 +2,7 @@ package com.atguigu.jenkins.test;
 
 public class ShowData {
 	
-	private String message = "Try data...";
+	private String message = "Try data...[Scheduler trigger...]";
 	
 	public String getMessage() {
 		return message;
